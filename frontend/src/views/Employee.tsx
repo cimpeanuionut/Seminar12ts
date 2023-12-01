22674b0b4a0f7b634213bfe446977fcb26f75f8e
cimpeanuionut40 <cimpeanuionut40@gmail.com>
2023-12-01 16:17:55 +0200

    add table

--- frontend/src/views/Employee.tsx
+++ frontend/src/views/Employee.tsx
@@ -1,20 +1,101 @@
 import { useEffect, useState } from "react";
-import {get} from "../api/Calls";
+import { get } from "../api/Calls";
 import { Employee } from "../models/Employee";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableFooter from '@mui/material/TableFooter';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import TablePaginationActions from "../components/TablePaginationActions";
+import { PaginationResponse } from "../models/PaginationResponse";
+import { TableHead } from "@mui/material";
 
-export default function EmployeesList(){
+export default function EmployeesList() {
 
-    const [employees, setEmployees] = useState<Employee[]>([])
+    const [employees, setEmployees] = useState<PaginationResponse<Employee>>({count: 0, rows: []})
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
 
     useEffect(() => {
-        getEmployees().then(d => setEmployees(d))
+        getEmployees().then(d => {setEmployees(d); })
     }, [])
 
-    async function getEmployees(){
-        return (await get("/employee")) as Employee[];        
+    async function getEmployees() {
+        return (await get("/employee")) as PaginationResponse<Employee>;
     }
+    
+    const emptyRows =
+        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - employees.rows.length) : 0;
 
-    return(
-       <h1>Employee page</h1>
-    )    
+    const handleChangePage = (
+        event: React.MouseEvent<HTMLButtonElement> | null,
+        newPage: number,
+    ) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (
+        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    ) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    return (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+            <TableHead>
+                <TableRow>
+                    <TableCell>Employee Name</TableCell>
+                    <TableCell>Employee SurName</TableCell>
+                    <TableCell>Employee Age</TableCell>
+                    <TableCell>Employee Occupation</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+              {(rowsPerPage > 0
+                ? employees.rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                : employees.rows
+              ).map((row) => (
+                <TableRow key={row.EmployeeId}>
+                  <TableCell align="left">
+                    {row.EmployeeName}
+                  </TableCell>
+                  <TableCell align="left">
+                    {row.EmployeeSurName}
+                  </TableCell>
+                  <TableCell align="left">
+                    {row.EmployeeAge}
+                  </TableCell>
+                  <TableCell align="left">
+                    {row.EmployeeOccupation}
+                  </TableCell>
+                </TableRow>
+              ))}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 53 * emptyRows }}>
+                  <TableCell colSpan={6} />
+                </TableRow>
+              )}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TablePagination
+                  rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                  colSpan={3}
+                  count={employees.count}
+                  rowsPerPage={rowsPerPage}
+                  page={page}                
+                  onPageChange={handleChangePage}
+                  onRowsPerPageChange={handleChangeRowsPerPage}
+                  ActionsComponent={TablePaginationActions}
+                />
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </TableContainer>
+      );
 }
